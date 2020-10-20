80dbdc49c8f9d6e848be8abd028c49ff5cef8c7c
Ian Edwards <EDWIAN004@myuct.ac.za>
2020-10-21 00:24:41 +0200

    Wrote read/write functionality, added some error checking, tag hunting and a test program.

--- nfcreader/nfcreader.py
+++ nfcreader/nfcreader.py
@@ -9,9 +9,12 @@ def USBConnect():
         print("Connected!")
     else:
         print("Error! NFC Reader not detected!")
+        sys.Exit()
 
 def Echo():
     output = nfc.Echo()
+    if (output != '5500'):
+        Reset()
     print("Sending echo to CR95HF... Result: ", output)
 
 def Select():
@@ -22,11 +25,47 @@ def SendReceive():
     output = nfc.SendReceive()
     print(output[0], output[1], "success")
 
-def main():
+def Reset():
+    nfc.ResetSPI()
+
+def main(): #Change how it starts... proper error check for USB error? Stop. etc.
     USBConnect()
+    Reset()
+    
+    
+    #nfc.SendReceive(b'02D202') #initiate
+    print(nfc.Select())
+    print(nfc.SendReceive()) #inventory command of tag.
     Echo()
-    Select()
-    SendReceive()
+    print("Welcome to NFC reader API demo!\n=========================================================")
+    print("1) Read a block from the tag.")
+    print("2) Write a block to the tag.")
+    print("3) Enter Tag hunting mode.")
+    print("4) Reset SPI connection (Reset).")
+    print("5) Exit.")
+    print('=========================================================')
+    while (input != '0'):
+        option = input("Please select an option from the menu:\n")
+        if (len(option) != 1 or not(option.isdigit())):
+            print("Incorrect option selected! Please try again.\n")
+        else:
+            option = int(option)
+            if (option == 1):
+                location = input("Please enter a block location:\n")
+                print(nfc.Read_Block(location))
+            if (option == 2):
+                location = input("Please enter a block location:\n")
+                data = input("Please enter 8 digit hex data value:\n") #Make this decimal or string eventually...
+                print(nfc.Write_Block(location, data))
+            if (option == 3):
+                nfc.ResetSPI()
+                nfc.continuousTagScan()
+            if (option == 4):
+                print("SPI connection reset.")
+                Reset()
+            if (option == 5):
+                print("Thank you for trying our API.")
+                sys.exit()
 
 
 
--- nfcreader/pylibCR95HF-master/pylibCR95HF/CR95HF.py
+++ nfcreader/pylibCR95HF-master/pylibCR95HF/CR95HF.py
@@ -2,9 +2,12 @@
 import ctypes
 from ctypes import *
 cr95hf = ctypes.CDLL('libCR95HF.so');
+import time
 #testlib = ctypes.CDLL('./testlib.so');
 
-#print cr95hf.CR95HFlib_USBConnect()
+#>>> Frame sent by the Host to CR95HF
+#<<< Frame sent by the CR95HF to the Host
+
 def USBConnect():
 	return cr95hf._Z20CR95HFlib_USBConnectv()
 
@@ -32,7 +35,7 @@ def Select(request = 'ISO15693'):
 	strAnswer= ctypes.create_string_buffer(b'\000',50)
 	strRequest=c_char_p(b'010D')
 	res=cr95hf._Z16CR95HFlib_SelectPcS_(strRequest,strAnswer)
-	return (res,strAnswer.value.decode("utf-8"))
+	return (res,strAnswer.value.decode("ASCII"))
 
 def SendReceive(request= b'260100'):
 	strAnswer= ctypes.create_string_buffer(b'\000',50)
@@ -40,18 +43,37 @@ def SendReceive(request= b'260100'):
 	res=cr95hf._Z21CR95HFlib_SendReceivePcS_(strRequest,strAnswer)
 	return (res,strAnswer.value.decode("utf-8"))
 
-def Read_Block(block=0):
-	strAnswer= ctypes.create_string_buffer(b'\000',50)
-	strRequest=c_uint(block)
-	res=cr95hf._Z20CR95HFlib_Read_BlockiPh(strRequest,strAnswer)
-	return (res,strAnswer.value.decode("utf-8"))
+def Read_Block(location='2', blocks = 1): #for now enter whole command... blocks is number of lines to read. Optional.
+	location = (3-len(location))*'0' + location #Enter location 1? Add two '0' so it is 001.
+	command = '022' + location #command 022001
+	command = bytes(str(command), 'utf-8')
+	answer = []
+	answer = SendReceive(command)
+	return (answer[0], answer[1])
+    	
+def Write_Block(location = '2',data = '00000000'): #remember to add check that data is not longer than 8 hex values long. And remember we need to convert data...
+	location = (2-len(location))*'0' + location #Enter location 1? Add one '0' so it is 01.
+	command = ''
+	command = '0221' + location + data
+	command = bytes(str(command), 'utf-8')
+	answer = []
+	answer = SendReceive(command)
+	return (answer[0], answer[1])
+    
+    
 
-def Write_Block(block, data):
-	strAnswer= ctypes.create_string_buffer(b'\000',50)
-	strData= ctypes.create_string_buffer(b'\000',50)
-	strRequest=c_uint(block)
-	res=cr95hf._Z21CR95HFlib_Write_BlockiPhS_(strRequest,strAnswer,strData)
-	return (res,strAnswer.value.decode("utf-8"))
+# def Read_Block(block=0):
+# 	strAnswer= ctypes.create_string_buffer(b'\000',50)
+# 	strRequest=c_uint(block)
+# 	res=cr95hf._Z20CR95HFlib_Read_BlockiPh(strRequest,strAnswer)
+# 	return (res,strAnswer.value)
+
+# def Write_Block(block, data):
+# 	strAnswer= ctypes.create_string_buffer(b'\000',50)
+# 	strData= ctypes.create_string_buffer(b'\000',50)
+# 	strRequest=c_uint(block)
+# 	res=cr95hf._Z21CR95HFlib_Write_BlockiPhS_(strRequest,strAnswer,strData)
+# 	return (res,strAnswer.value.decode("utf-8"))
 
 
 def FieldOff():
@@ -75,9 +97,101 @@ def SendNSSPulse():
 	return (res,strAnswer.value.decode("utf-8"));
 
 
-def STCmd(request=b'010202010D'):
+def STCmd(request=b'010803620100'): #default do protocol select
 	strAnswer= ctypes.create_string_buffer(b'\000',50)
 	strRequest=c_char_p(request)
 	res=cr95hf._Z15CR95HFlib_STCmdPcS_(strRequest,strAnswer)
 	return (res,strAnswer.value.decode("utf-8"))
-	
+
+# def continuousTagScan():
+# 	result = []
+# 	tagDetected = False
+# 	try:
+# 		while (tagDetected == False): #loop forever until tag found
+# 			result = STCmd(b'01070E0A21007901180020606064743F08') #wake up event due to tag presence. Not found? FD102 returned
+# 			if (result[1] == '000102'):
+# 				if (lookForTag() == True): #Scan specifically on 15693 protocol.
+# 					tagDetected = True
+# 					return tagDetected #return tag found, end loop.
+				
+# 	except KeyboardInterrupt:
+# 		print("Scanning cancelled.")
+# 		return False #tag not found.
+
+# def lookForTag(): #look for generic tag - no specific protocol...
+# 	#Assign constants to hex values later...
+# 	#Also maybe check responses from each command from IC?
+# 	t0 = time.time()
+# 	found = False
+# 	while (found == False):
+# 		pollVal = [] #Clear pointers passed into commands.
+# 		ReceiveVal = []
+
+# 		if (time.time() - t0 > 5): #Timeout after 5 seconds of detecting generic tag.
+# 			break
+
+# 		echo = Echo() #Establish contact with CR95HF IC.
+# 		STCmd(b'01090468010710') #Reset HF2RF
+# 		STCmd(b'01090468010700') #Set HF2RF
+# 		pollVal = STCmd(b'01070E0B21007901180020606000FF3F01') #CR95HF enters low consumption Wait for Event mode (Hibernate, sleep, tag detection etc)
+# 		#pollVal = STCmd(b'01070E0A21007901180020606064743F08') software's generic idle state till woken up by tag or IRQ pin
+# 		time.sleep(0.005)
+# 		Select(b'010D')
+# 		time.sleep(0.005)
+# 		STCmd(b'02A00126')
+# 		time.sleep(0.005)
+# 		ReceiveVal = SendReceive(b'0226') #Reset to ready. If response of 8700, no tag.
+		
+# 		if (pollVal[1] == '000102' and ReceiveVal[0] != 4):
+# 			found = True;
+
+# 	if (found):
+# 		print(ReceiveVal, "\nFound tag with protocol 15693.")
+# 		STCmd(b'01090468010710')
+# 		ResetSPI()
+# 		Select(b'010D')
+# 		return True
+# 	else: #unnecessary else?
+# 		#print("No tag found.")
+# 		return False
+
+def continuousTagScan(): #attempt nr 2
+	found = False
+	t0 = time.time()
+	print("|", end = '')
+	while (found == False):
+		pollVal = [] #Clear pointers passed into commands.
+		ReceiveVal = []
+
+		if (time.time() - t0 > 5): #Timeout after 5 seconds of detecting generic tag.
+			print("|")
+			break
+
+		if (int(time.time()) > t0):
+			print("=", end = '', flush = True)
+		echo = Echo() #Establish contact with CR95HF IC.
+		STCmd(b'01090468010710') #Reset HF2RF
+		STCmd(b'01090468010700') #Set HF2RF
+		pollVal = STCmd(b'01070E0B21007901180020606000FF3F01') #CR95HF enters low consumption Wait for Event mode (Hibernate, sleep, tag detection etc)
+		#pollVal = STCmd(b'01070E0A21007901180020606064743F08') software's generic idle state till woken up by tag or IRQ pin
+		time.sleep(0.005)
+		Select(b'010D')
+		time.sleep(0.005)
+		STCmd(b'02A00126')
+		time.sleep(0.005)
+		ReceiveVal = SendReceive(b'0226') #Reset to ready. If response of 8700, no tag.
+		
+		if (pollVal[1] == '000102' and ReceiveVal[0] != 4):
+			found = True;
+			print("|")
+
+	if (found):
+		print("Found a flipping tag.")
+		return True
+	else:
+		print("big oof. No tag.")
+		return False
+
+def elapsedTime(t0):
+	t1 = time.time()
+	return t1 - t0
