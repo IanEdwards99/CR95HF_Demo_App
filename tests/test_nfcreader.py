1ed24362158f82eaccb35ec462e896f04cd8cb29
Ian Edwards <EDWIAN004@myuct.ac.za>
2020-11-05 00:50:27 +0200

    Documentation.

--- tests/test_nfcreader.py
+++ tests/test_nfcreader.py
@@ -1,37 +1,106 @@
 #!/usr/bin/env python
-
+# Authors: Ian Edwards and Agoritsa Spirakis
+# MIT License
+# PyTests for nfcreader API for CR95HF module.
 """Tests for `nfcreader` package."""
 
 import pytest
+import sys
 
-from click.testing import CliRunner
+from nfcreader import nfcreader as nfc
 
-from nfcreader import nfcreader
-from nfcreader import cli
+def test_USBConnect():
+    x = nfc.USBConnect()
+    assert x == 1, "Error with USBConnect."
+
+def test_Echo():
+    """Test Echo function"""
+    x= nfc.Echo()
+    assert x[0] == 5, "Echo test failed, MCU response: " + str(x[0])
+
+def test_MCUrev():
+    x = nfc.MCUrev()
+    assert x[0] == 5, "MCUrev test failed, response: " + str(x[0])
+
+def test_getInterfacePinState():
+    x = nfc.getInterfacePinState()
+    assert x[0] == 5, "InterefacePinState fetch test failed, response: " + str(x[0])
+
+def test_ReadBlock():
+    x = nfc.Read_Block("a")
+    assert x[0] == "9", "ReadBlock fetch test failed, response: " + str(x[0])
+
+def test_SendReceive():
+    x = nfc.SendReceive()
+    assert x[0] == 5, "SendReceive fetch test failed, response: " + str(x[0])
+
+def test_Select():
+    x = nfc.Select()
+    assert x[0] == 5, "Select fetch test failed, response: " + str(x[0])
+
+def test_Write_Block():
+    x = nfc.Write_Block("a")
+    assert x[0] == "9", "Write_Block fetch test failed, response: " + str(x[0])
+
+def test_Idn():
+    x = nfc.Idn()
+    assert x[0] == 5, "Idn test failed, response: " + str(x[0])
+
+def test_FieldOff():
+    x = nfc.FieldOff()
+    assert x[0] == 5, "FieldOff test failed, response: " + str(x[0])
+
+def test_ResetSPI():
+    x = nfc.ResetSPI()
+    assert x[0] == 5, "ResetSPI test failed, response: " + str(x[0])
+    
+def test_SendIRQPulse():
+    x = nfc.SendIRQPulse()
+    assert x[0] == 5, "SendIRQPulse test failed, response: " + str(x[0])
+    
+def test_SendNSSPulse():
+    x = nfc.SendNSSPulse()
+    assert x[0] == 5, "NSSPulse test failed, response: " + str(x[0])
+
+def test_STCmd():
+    x = nfc.STCmd()
+    assert x[0] == 5, "STCmd test failed, response: " + str(x[0])
+    
+def test_initiate():
+    x = nfc.initiate()
+    assert x[0] == 5, "initiate test failed, response: " + str(x[0])
+
+def test_ResetToReady():
+    x = nfc.ResetToReady()
+    assert x[0] == 5, "ResetToReady test failed, response: " + str(x[0])
+
+def test_GetSysInfo():
+    x = nfc.GetSysInfo()
+    assert x[0] == 5, "GetSysInfo test failed, response: " + str(x[0])
+    
+def test_idleForTag():
+    x = nfc.idleForTag()
+    assert x[0] == 5, "idleForTag test failed, response: " + str(x[0])
+
+def test_tagHunting():
+    x = nfc.tagHunting()
+    assert x == False, "tagHunting test failed, response: " + str(x)
+
+def test_extractPayload():
+    x = nfc.extractPayload("80080000000005DA9801")
+    assert x == "00000005", "extractPayload test failed, response: " + x
+
+def test_dec2Hex():
+    x = nfc.decToHex("400")
+    assert x == "190", "Dec to Hex test failed, response: " + x
+
+def test_hexToDec():
+    x = nfc.hexToDec("190")
+    assert x == 400, "hexToDec test failed, response: " + str(x)
+    
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'nfcreader.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
